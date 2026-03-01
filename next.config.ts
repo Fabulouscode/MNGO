002047b50c6cf42b9e172b8124025a0bf144028d
ulagh fabian <fabian@mainstack.com>
2026-03-01 10:52:16 +0100

    Auto deploy Namecheap GitHub Action created

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // You can optionally add a trailing slash if needed for cPanel hosting
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export with Next.js images
+  }
 };
 
 export default nextConfig;
